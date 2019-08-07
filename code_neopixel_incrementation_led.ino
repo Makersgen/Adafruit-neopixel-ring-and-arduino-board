bbb88687561166802883bc90d2eb4b0daa9aea45
Makersgeneration <makersgeneration@gmail.com>
2019-08-06 21:19:26 -0400

    Add files via upload

---
+++ code_neopixel_incrementation_led.ino
@@ -0,0 +1,36 @@
+
+#include <Adafruit_NeoPixel.h>
+
+ 
+#define PIN 5
+ 
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(16, PIN); // create a objec "pixel"
+     
+uint32_t color  = 0xFFFF00; // color red
+
+ 
+void setup() {
+
+  pixels.begin();
+  pixels.setBrightness(50); 
+
+}
+
+void loop()
+{
+    
+    int a=0 ;
+    
+    for (a; a<16; a++) // loop 16 times to shift the light on the ring
+    {
+   
+      pixels.setPixelColor(a, color);
+      pixels.show();
+      delay(100);
+      pixels.setPixelColor(a, 0);
+    }
+    
+}
+
+
+  
