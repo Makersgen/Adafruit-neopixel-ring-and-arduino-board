5f1dab4413560a50e84d525567a08b336f66bb15
Makersgeneration <makersgeneration@gmail.com>
2019-08-06 21:18:47 -0400

    Add files via upload

---
+++ code_neopixel_ring_random_yellow_.ino
@@ -0,0 +1,33 @@
+
+#include <Adafruit_NeoPixel.h>
+
+ 
+#define PIN 5
+ 
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(16, PIN);
+     
+uint32_t color  = 0xFFFF00; 
+
+ 
+void setup() {
+
+  pixels.begin();
+  pixels.setBrightness(50); 
+
+}
+
+void loop()
+{
+    uint8_t  i;
+   
+ 
+    i = random(16);
+    pixels.setPixelColor(i, color);
+    pixels.show();
+    delay(100);
+    pixels.setPixelColor(i, 0);
+    
+}
+
+
+  
